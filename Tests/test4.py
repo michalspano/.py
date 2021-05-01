ed8e53080928882e45464d489cebdff053d3f7bd
michalspano <speko.spano@gmail.com>
2021-05-01 14:45:42 +0200

    1/5/2021 v2

--- Tests/test4.py
+++ Tests/test4.py
@@ -1,12 +1,6 @@
 import tkinter
-from PIL import ImageTk, Image
 canvas = tkinter.Canvas()
 canvas.pack()
 
-photo = Image.open("/Users/michalspano/Google Drive/P-4th Grade/Revision exercises/flowers/flower1.png")
-resized = photo.resize((300, 225), Image.ANTIALIAS)
-new_pic = ImageTk.PhotoImage(resized)
-
-canvas.create_image(100, 100, image=new_pic)
-
-canvas.mainloop()
+tk = canvas.create_rectangle(10, 10, 100, 100, fill="red")
+print(not tk)
