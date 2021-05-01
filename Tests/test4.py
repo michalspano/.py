010a04a71eddff6f1235df85ad9cb007ba345256
michalspano <speko.spano@gmail.com>
2021-05-01 11:57:17 +0200

    1/5/2021 v1

--- Tests/test4.py
+++ Tests/test4.py
@@ -1,8 +1,12 @@
-print("Test 4")
-n = ["a" if i % 2 == 0 else "b" for i in range(5)]
-n.sort(reverse=True)
+import tkinter
+from PIL import ImageTk, Image
+canvas = tkinter.Canvas()
+canvas.pack()
 
-i = 0
-while i < len(n):
-    print(n[i])
-    i += 1
+photo = Image.open("/Users/michalspano/Google Drive/P-4th Grade/Revision exercises/flowers/flower1.png")
+resized = photo.resize((300, 225), Image.ANTIALIAS)
+new_pic = ImageTk.PhotoImage(resized)
+
+canvas.create_image(100, 100, image=new_pic)
+
+canvas.mainloop()
