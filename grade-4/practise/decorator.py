c9ea318d40b7f1b967b8c8bd5807544f08a11bae
michalspano <speko.spano@gmail.com>
2021-12-30 22:46:09 +0100

    Update decorators in Py

--- grade-4/practise/decorator.py
+++ grade-4/practise/decorator.py
@@ -1,37 +1,53 @@
-# Store credentials in a dictionaty
-creds: dict = {"name": "Michal", "age": 20}
+# ***For educational purposes only***
+
+from sys import exit
+from csv import DictReader
 
 
-# Create an adult check decorator
-def adult_check(func):
+# Load CSV source file
+def load_people(path: str = 's.csv') -> dict or None:
+    temp: dict = {}
+    # Catch invalid input file
+    try:
+        with open(path) as f:
+            reader = DictReader(f)
+            for person in reader:
+                temp[person['name']] = int(person['age'])
 
-    # Create local inner wrapper
-    def wrapper():
+        # Return the populated dictionary of people
+        return temp
 
-        # Provide simple logics to decide upon the function
-        if creds['age'] >= 18:
-            func()
-        else:
-            print("Not old enough")
+    # Return None if not found
+    except FileNotFoundError:
+        return None
 
-    # Return the wrapper
+
+people: dict = load_people()
+
+# Check if the dict is not empty
+if not people:
+    print('No people found.')
+    exit(1)
+
+
+# Define a decorator that will check whether the person is an adult
+# If so, return the function
+# If not, return a message
+def check_adult(func):
+    def wrapper(*args, **kvargs):
+        name: str = args[0]
+        if people[name] >= 18:
+            return func(*args, **kvargs)
+        print(f'You have to wait {18 - people[name]} years to be an adult.')
     return wrapper
 
 
-# Assign the decorator to the function
-@adult_check
-def prompt():
-    # Print out prompt statement
-    print(f"Helo {creds['name']}, age: {creds['age']}"
-          f"\nYou are a certified adult!")
+@check_adult  # Assigning the decorator
+def grab_beer(name: str):
+    print(f'Have a beer with us, {name}!')
 
 
-# Define main core func.
-def main():
-    prompt()
-
-
-# Define main executable
-if __name__ == "__main__":
-    main()
+# Iterate over all people
+for name in people:
+    grab_beer(name)
 
